c23ecf8bba8ded05a1452dd120e9df9d461a64d3
hapinesszj <wisdomzj365@gmail.com>
2022-07-10 11:15:26 +0800

    feat(element-plus): 集成element-plus UI库

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">测试11</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <img src="./assets/logo.png">
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
-<script>
-export default {
-  data      () {
-    return {
 
-    }
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
